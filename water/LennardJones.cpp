8337493c0f5b92e741c0018075ed219547cb9049
Michele Invernizzi <inve.michele@gmail.com>
2020-07-13 17:11:33 +0200

    updated to MPI NeighborLists

--- water/LennardJones.cpp
+++ water/LennardJones.cpp
@@ -106,11 +106,11 @@ LennardJones::LennardJones(const ActionOptions&ao):
 
   addValueWithDerivatives(); setNotPeriodic();
   if(gb_lista.size()>0) {
-    if(doneigh)  nl.reset( new NeighborList(ga_lista,gb_lista,dopair,pbc,getPbc(),nl_cut,nl_st) );
-    else         nl.reset( new NeighborList(ga_lista,gb_lista,dopair,pbc,getPbc()) );
+    if(doneigh)  nl.reset( new NeighborList(ga_lista,gb_lista,serial,dopair,pbc,getPbc(),comm,nl_cut,nl_st) );
+    else         nl.reset( new NeighborList(ga_lista,gb_lista,serial,dopair,pbc,getPbc(),comm) );
   } else {
-    if(doneigh)  nl.reset( new NeighborList(ga_lista,pbc,getPbc(),nl_cut,nl_st) );
-    else         nl.reset( new NeighborList(ga_lista,pbc,getPbc()) );
+    if(doneigh)  nl.reset( new NeighborList(ga_lista,serial,pbc,getPbc(),comm,nl_cut,nl_st) );
+    else         nl.reset( new NeighborList(ga_lista,serial,pbc,getPbc(),comm) );
   }
 
   requestAtoms(nl->getFullAtomList());
